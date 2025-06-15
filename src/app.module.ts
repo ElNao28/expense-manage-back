921c05a9fa1b0c94bcbd738baffb779390b93162
LuisJair <jair.escalante@phs.mx>
2025-06-15 17:33:39 -0600

    add envitoments config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { ExpensesModule } from './expenses/expenses.module';
 import { HistoryModule } from './history/history.module';
 import { CashModule } from './cash/cash.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ExpensesModule, HistoryModule, CashModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      port: +process.env.PORT_BD,
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,
+      database: process.env.NAME_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ExpensesModule,
+    HistoryModule,
+    CashModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
